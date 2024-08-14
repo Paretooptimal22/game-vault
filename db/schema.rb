95ba16f4edc5a6ab2fae6fd7aadf4ece4646db0f
yuta asakura <yuta.asakura.se@gmail.com>
2024-08-14 13:42:33 +0900

    changed status from boolean to array

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_13_024427) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_14_043444) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -41,9 +41,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_13_024427) do
     t.bigint "user_id", null: false
     t.date "start_date"
     t.date "end_date"
-    t.boolean "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status", default: ["pending"], array: true
     t.index ["listing_id"], name: "index_rentals_on_listing_id"
     t.index ["user_id"], name: "index_rentals_on_user_id"
   end
