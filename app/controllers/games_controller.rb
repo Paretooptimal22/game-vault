1082bd0beb3d03d2373b28d83aced5d484f0249b
kevin young <you.k2208@gmail.com>
2024-08-15 17:57:54 +0900

    fixed

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,11 +1,14 @@
 class GamesController < ApplicationController
 
   def index
-    @games = Game.where.associated(:listings)
-
+    @games = Game.where.associated(:listings).uniq
+    if params[:query].present?
+      @games = Game.search_by_game_genres_platforms_and_name(params[:query])
+    end
   end
 
   def show
-
+    @game = Game.find(params[:id])
+    @rental = Rental.new
   end
 end
