6b7634785e986f5038caef1a5fd53c51fbbf762a
kevin young <you.k2208@gmail.com>
2024-08-15 13:30:42 +0900

    updated show and index and listing controller

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,9 +1,12 @@
 class ListingsController < ApplicationController
   def index
-    @listings = Listing.all
+    @rentals = Rental.all
+    @listings = Rental.where(@rentals.each { |rental| rental.status.pending? })
+    # @listings = @listings.where(@listings.each { |listing| listing.status != listing.approved })
   end
 
   def show
+    @listings = Listing.all
     @listing = Listing.find(params[:id])
     @rental = Rental.new
   end
